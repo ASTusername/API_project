39d5f66f78960570815223d373d69f4edb83f08c
Alexandr Trifonov <ast89646421790@yandex.ru>
2024-03-13 19:46:06 +0300

    fix config

--- src/test/java/config/UserConfig.java
+++ src/test/java/config/UserConfig.java
@@ -9,32 +9,7 @@ import java.net.URL;
 })
 public interface UserConfig extends Config {
     @Key("userName")
-    String getUserName();@Config.Sources({"classpath:${env}.properties"})
-    public interface WebConfig extends Config {
-        @Key("baseUrl")
-        @DefaultValue("https://www.rusmuseum.ru/")
-        String baseUrl();
-
-        @Key("browser")
-        @DefaultValue("chrome")
-        String browser();
-
-        @Key("browserVersion")
-        @DefaultValue("100.0")
-        String browserVersion();
-
-        @Key("browserSize")
-        @DefaultValue("1920x1080")
-        String browserSize();
-
-        @Key("isRemote")
-        @DefaultValue("false")
-        boolean isRemote();
-
-        @Key("remoteUrl")
-        @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
-        URL remoteUrl();
-    }
+    String getUserName();
     @Key("password")
     String getPassword();
     @Key("userId")
