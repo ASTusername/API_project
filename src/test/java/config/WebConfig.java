a1f8e23e288652f59926940d4cb42fd7ecf208d4
Alexandr Trifonov <ast89646421790@yandex.ru>
2024-03-16 23:08:29 +0300

    fix config

--- src/test/java/config/WebConfig.java
+++ src/test/java/config/WebConfig.java
@@ -12,19 +12,15 @@ public interface WebConfig extends Config {
     @Key("browser")
     @DefaultValue("chrome")
     String browser();
-
     @Key("browserVersion")
     @DefaultValue("122.0")
     String browserVersion();
-
     @Key("browserSize")
     @DefaultValue("1920x1080")
     String browserSize();
-
     @Key("isRemote")
     @DefaultValue("false")
     boolean isRemote();
-
     @Key("remoteUrl")
     @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
     String remoteUrl();
